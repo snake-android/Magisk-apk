975807d2b16fe8b111269f081aa45448c7964cfa
Snake Android <felyaanika@gmail.com>
2025-02-06 00:05:04 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+rootProject.name = "Magisk"
+include(":app:apk", ":app:core", ":app:shared", ":app:stub", ":app:test", ":native")
